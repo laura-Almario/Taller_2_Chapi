aa38edd827bf70bae678ecfc9254b11f41b10183
aerodriguezn <135923287+aerodriguezn@users.noreply.github.com>
2023-07-16 20:06:25 -0500

    Add files via upload

---
+++ Scripts/Modelo_Árbol_RefresiónLineal.R
@@ -0,0 +1,49 @@
+#Modelo de Regresión Lineal
+bd <- readRDS("C:/Users/anama/OneDrive - Universidad de los andes/BIG DATA/TALLER 2/bd_NA.rds")
+
+# Subconjunto de datos train
+train <- subset(bd, !is.na(price))
+
+# Subconjunto de datos test
+test <- subset(bd, is.na(price))
+
+train_s<- train
+test_s <- test
+
+#Modelo de arboles
+library(caret)
+library(sf)
+
+test <-test_s  %>% 
+  mutate(sample="test")
+train<-train_s  %>% 
+  mutate(sample="train")
+
+train_reducido <- train %>% sample_n(size = 10286, replace = FALSE)
+
+if (anyNA(test)) {}
+
+fitControl<-trainControl(method ="cv", number=5)
+
+set.seed(2023)
+tree <- train(
+  log(price) ~surface_total + bedrooms + property_type + 
+    property_type + Parking + Estudio + dist_bus_bogota + 
+    dist_park_bogota,
+  data = train_reducido,
+  method = "lm",
+  trControl = fitControl,
+  tuneLength=5
+)
+
+#test <- bd %>% filter(sample = "test")
+test$pred_tree<-predict(tree, test)
+
+head(test)
+
+test_data <- test   %>% st_drop_geometry()  %>% mutate(pred_tree=exp(pred_tree))
+head(test_data)
+
+submit<- test_data[, c('property_id','pred_tree')]
+submit <- submit  %>% rename(price=pred_tree)
+write.csv(submit,"Tree_Lineal.csv",row.names=FALSE)
